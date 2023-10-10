0ae1aef202f58110c67da8377d77a0a508a32048
Fanttatraieste <118627923+Fanttatraieste@users.noreply.github.com>
2023-10-10 16:25:35 +0300

    Update app.js

--- app.js
+++ app.js
@@ -16,7 +16,7 @@ const app = express();
 
 app.use(
   cors({
-    origin: 'https://fanttatraieste.github.io/RetroFootballBackend/',
+    origin: 'http://localhost:5173',
     methods: ['GET', 'POST', 'DELETE', 'PATCH'],
   }),
 );
